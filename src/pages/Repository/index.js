6e0fde2306c1a22249e9021be7fbd42a94eafc01
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-28 16:41:24 -0300

    showing repos

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -41,6 +43,20 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
-    return <h1>Repository</h1>;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositótios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
